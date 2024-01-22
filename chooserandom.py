0339ec1611cd093c3bb902396410347e263dc09c
DauranRaza <157312125+DauranRaza@users.noreply.github.com>
2024-01-23 03:55:47 +0500

    Add files via upload

---
+++ chooserandom.py
@@ -0,0 +1,3 @@
+import random
+print("choose a random number between 16 and 272")
+print(random.randint(16, 272))
---
+++ ps0.py
@@ -0,0 +1,8 @@
+import math
+
+x = int(input("enter number x"))
+y = int(input ("enter number y"))
+
+print("Exponential is ", x**y)
+
+print(math.log(x))
