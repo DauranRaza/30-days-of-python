7ade832a2c7bcf72e2ced58351ee4965a0993f25
DauranRaza <157312125+DauranRaza@users.noreply.github.com>
2024-01-26 09:40:10 +0500

    Add files via upload

---
+++ Excerciselevel1.py
@@ -0,0 +1,19 @@
+x = 3
+y = 4
+
+print("Addition is ", x+y)
+
+print("Subtraction is ", x-y)
+
+print("multiplication is ", x*y)
+
+print("Modulus is ", x%y)
+
+print("Division is ", x/y)
+
+print("Exponential is ", x**y)
+
+print("Floor division operator is ", x//y)
+
+
+
