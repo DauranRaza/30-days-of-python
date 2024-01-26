cef7e09ca91cc31ca2e37b2a29ba5c7cb4659280
DauranRaza <157312125+DauranRaza@users.noreply.github.com>
2024-01-26 09:48:01 +0500

    Create helloworld.py

---
+++ day_1/helloworld.py
@@ -0,0 +1,49 @@
+# check python version you are using
+
+version_file_path
+
+#open python shell and do following operations. the operands are 3 and 4.
+# addition, subtraction, multiplication, modulus, division, exponential, floow division operator
+
+x = 3
+y = 4
+
+print("Addition is ", x+y)
+
+print("Subtraction is ", x-y)
+
+print("multiplication is ", x*y)
+
+print("Modulus is ", x%y)
+
+print("Division is ", x/y)
+
+print("Exponential is ", x**y)
+
+print("Floor division operator is ", x//y)
+
+# write strings on python shell. the strings are the following:
+# your name
+# yourfamily name
+# your country
+# I am enjoying 30 days in python
+
+name = input("What is your name ")
+familyname = input("What is your Family name ")
+country = input("What is your country ")
+print("I am enjoying 30 days of python")
+
+#Check data types of following data
+# 10, 9.8, 3.14
+#4-4j
+#['Asabeneh','python','Finland']
+#your name, your family name, your country
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh','python','Finland']))
+print(type(name))
+print(type(familyname))
+print(type(country))
