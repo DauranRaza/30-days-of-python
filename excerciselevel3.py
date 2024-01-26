2e63d3eec4e649b239953ed25ca009b77aa655ce
DauranRaza <157312125+DauranRaza@users.noreply.github.com>
2024-01-26 10:23:43 +0500

    Add files via upload

---
+++ excerciselevel3.py
@@ -0,0 +1,30 @@
+# Write an example for different python data types such as Numeric(Int, float, complete), string, boolean,
+# list, tuple, set and dictionary
+
+x = 3
+print("x is ", type(x))
+
+y = 3.3
+print("y is ", type(y))
+
+z = 3j
+print("z is ", type(z))
+
+s = "abc"
+print("s is ", type(s))
+
+b = True
+print("b is ", type(b))
+
+l = ["banana", "apple", "orange"]
+print("l is ", type(l))
+
+t = ("banana", "apple", "orange")
+print("t is ", type(t))
+
+e = {"banana", "apple", "orange"}
+print("e is ", type(e))
+
+d = {"name" : "john", "age" : 36}
+print(d)
+print ("d is ", type(d))
