61b4868eed1ff50a5a8c99eef0e35b517d5e990e
DauranRaza <157312125+DauranRaza@users.noreply.github.com>
2024-02-12 13:11:06 +0500

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,20 @@
+# Declare some variables
+
+age = 30
+height = 5.8
+compl = 1+3j
+
+# write a program to calculate area of triangle
+
+base = int(input("Enter base: "))
+h_t = int(input("Enter height: "))
+area = 0.5 * base * h_t
+print("The area of th triangle is: ", area)
+
+# calculate parameter
+
+a = int(input("Enter Side a: "))
+b = int(input("Enter side b: "))
+c = int(input("Enter Side c: "))
+perimeter = (a + b + c)
+print("The perimeter of the triangle is ", perimeter)
